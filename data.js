de2e430e6f6ce70248ce26556004f508b341ef3d
Stephen Said <stephen7776@gmail.com>
2026-03-28 12:52:55 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,95 @@
+// SP MOTOR - KIOSK DATABASE
+// Only edit the items below. Make sure every item has a comma at the end (except the last one)!
+
+const featuredItems = [
+  {
+    type: "product",
+    title: "Helmet Pro",
+    link: "https://www.google.com/search?igu=1",
+    image:
+      "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?w=800&q=80",
+  },
+  {
+    type: "product",
+    title: "ICON - Airform Counterstrike",
+    link: "https://www.partseurope.eu/en/product/airformtm-counterstrike-mips-r-helmet/010114137",
+    video: "https://youtu.be/jUiuPZkJUgU?si=tSssf6LvHms3fAHJ",
+  },
+  {
+    type: "gallery",
+    title: "Showroom Front",
+    link: "#",
+    image:
+      "https://images.unsplash.com/photo-1558981806-ec527fa84c39?w=800&q=80",
+  },
+  {
+    type: "product",
+    title: "Riding Gloves",
+    link: "https://www.google.com/search?igu=1",
+    image:
+      "https://images.unsplash.com/photo-1515552726023-7125c8d07fb3?w=800&q=80",
+  },
+  {
+    type: "gallery",
+    title: "New Bikes Arrival",
+    link: "#",
+    image:
+      "https://images.unsplash.com/photo-1449426468159-d96dbf08f19f?w=800&q=80",
+  },
+  {
+    type: "product",
+    title: "Airform Counterstrike Helmet",
+    link: "https://www.partseurope.eu/en/product/airformtm-counterstrike-mips-r-helmet/010114137",
+    image:
+      "https://images.unsplash.com/photo-1558981359-219d6364c9c8?w=800&q=80",
+  },
+  {
+    type: "gallery",
+    title: "Workshop Area",
+    link: "#",
+    image:
+      "https://images.unsplash.com/photo-1605335198226-c172a1e16fdf?w=800&q=80",
+  },
+  {
+    type: "product",
+    title: "Boots Alpine",
+    link: "https://www.google.com/search?igu=1",
+    image:
+      "https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf?w=800&q=80",
+  },
+  {
+    type: "gallery",
+    title: "Customer Ride Out",
+    link: "#",
+    image:
+      "https://images.unsplash.com/photo-1558980394-4c7c9299fe96?w=800&q=80",
+  },
+  {
+    type: "gallery",
+    title: "Engine Detailing",
+    link: "#",
+    image:
+      "https://images.unsplash.com/photo-1568772585407-9361f9bf3a87?w=800&q=80",
+  },
+  {
+    type: "product",
+    title: "Leather Jacket Pro",
+    link: "https://www.google.com/search?igu=1",
+    image:
+      "https://images.unsplash.com/photo-1520113524584-36109315d909?w=800&q=80",
+  },
+  {
+    type: "gallery",
+    title: "Custom Builds",
+    link: "#",
+    image:
+      "https://images.unsplash.com/photo-1590507963334-a690e3abdbf6?w=800&q=80",
+  },
+  {
+    type: "product",
+    title: "Performance Tires",
+    link: "https://www.google.com/search?igu=1",
+    image:
+      "https://images.unsplash.com/photo-1580274455191-1c62238fa333?w=800&q=80",
+  },
+];
